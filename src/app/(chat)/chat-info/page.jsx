aa99e3dddbce6d55a5e8474cd03f722541b4eae9
heesu21 <gmltncpfl1@naver.com>
2024-11-15 20:17:06 +0900

    feat: tabs작업중

--- src/app/(chat)/chat-info/page.jsx
+++ src/app/(chat)/chat-info/page.jsx
@@ -1,25 +1,13 @@
 'use client';
-import React, { useState } from 'react';
-import { Tabs } from 'antd';
 import ChatInfo from '@/components/ChatInfo/ChatInfo';
 import ExplainWord from '@/components/ChatInfo/ExplainWord';
 
 const ChatInfoPage = () => {
-  const [activeTabKey, setActiveTabKey] = useState('1'); // 초기 활성 탭은 ChatInfo
-
-  const handleTabChange = (key) => {
-    setActiveTabKey(key); // 탭 변경 시 activeTabKey 업데이트
-  };
-
   return (
-    <Tabs activeKey={activeTabKey} onChange={handleTabChange}>
-      <Tabs.TabPane tab="Chat Info" key="1">
-        <ChatInfo onExplainClick={() => setActiveTabKey('2')} /> {/* 퇴직금 클릭 시 ExplainWord로 이동 */}
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="Explain Word" key="2">
-        <ExplainWord />
-      </Tabs.TabPane>
-    </Tabs>
+    <div>
+      <ChatInfo />
+      <ExplainWord />
+    </div>
   );
 };
 
