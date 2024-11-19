1ba1a8cbc4650bb8fa22c0405de452cb63bb47ae
Joohwan Seo <songqkaoswe@naver.com>
2024-11-19 17:22:50 +0900

    feat: 홈 화면 추가로 인한 노출 페이지 변경

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,61 +1,16 @@
 'use client';
-import React, { useState } from 'react';
-import AllChatPage from './(chat)/all-chat/page';
-import ChatInfoPage from './(chat)/chat-info/page';
-import ChattingPage from './(chat)/live-chat/page';
-import SearchBarPage from './(chat)/search-bar/page';
-import SideBarPage from './(chat)/side-bar/page';
-import BasicSalary from '../components/ChatInfo/basicSalary';
-import EmploymentContract from '../components/ChatInfo/employmentxContract';
-import Specification from '../components/ChatInfo/specification';
-import SeverancePay from '../components/ChatInfo/severancePay';
 
-const Chatpage = () => {
-  const [currentInfoComponent, setCurrentInfoComponent] = useState(null);
+import Search from '@/components/SearchBar/Search';
+import SideBar from '@/components/SideBar/SideBar';
+import React from 'react';
 
-  const handleKeywordClick = (componentName) => {
-    setCurrentInfoComponent(componentName);
-  };
-
-  const handleBackToChatInfo = () => {
-    setCurrentInfoComponent(null);
-  };
-
+function HomePage() {
   return (
-    <div className="h-screen flex flex-col lg:flex-row ">
-      <aside className="lg:w-20 lg:h-full lg:fixed lg:left-0 lg:top-0 lg:block hidden">
-        <SideBarPage />
-      </aside>
-
-      <div className="lg:hidden fixed bottom-0 left-0 w-full bg-white flex justify-around py-2 border-t">
-        <SideBarPage />
-      </div>
-
-      <div className="flex flex-col flex-grow lg:ml-20">
-        <header className="w-full">
-          <SearchBarPage />
-        </header>
-
-        <div className="flex flex-grow">
-          <section className="w-full md:w-[18%] border-r hidden md:block">
-            <AllChatPage />
-          </section>
-
-          <section className="flex-grow">
-            <ChattingPage onKeywordClick={handleKeywordClick} />
-          </section>
-
-          <aside className="hidden lg:block w-[30%] border-l">
-            {currentInfoComponent === 'SeverancePay' && <SeverancePay onBack={handleBackToChatInfo} />}
-            {currentInfoComponent === 'BasicSalary' && <BasicSalary onBack={handleBackToChatInfo} />}
-            {currentInfoComponent === 'EmploymentContract' && <EmploymentContract onBack={handleBackToChatInfo} />}
-            {currentInfoComponent === 'Specification' && <Specification onBack={handleBackToChatInfo} />}
-            {!currentInfoComponent && <ChatInfoPage onKeywordClick={handleKeywordClick} />}
-          </aside>
-        </div>
-      </div>
+    <div>
+      <SideBar />
+      <Search />
     </div>
   );
-};
+}
 
-export default Chatpage;
+export default HomePage;
